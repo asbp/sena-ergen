5f37211f33a0b912f558d5a1f3ba49a72f00c415
asbp <asatriob@gmail.com>
2024-09-30 18:46:23 +0700

    fix selectinput

--- components/SelectInput.vue
+++ components/SelectInput.vue
@@ -1,13 +1,25 @@
 <script lang="ts" setup>
-import type { SelectInputProps } from '~/utils/types.ts';
+import type { SelectInputProps, SelectOption } from '~/utils/types.ts';
 
 const model = defineModel();
 const slots = useSlots();
-
-const optionsFromSlot = computed(() => filterSlots(slots, 'option'));
-
-// Props for this component is the same as the props for the native select element.
 const props = defineProps<SelectInputProps>();
+
+const optionsComputed = computed(() => {
+  const options = props.options ?? [];
+  let optionsResult: SelectOption[] = [];
+
+  for (const option of options) {
+    // If the option is a primitive value, create an object with the value as the key and the value as the value.
+    if (typeof option === 'string' || typeof option === 'number' || typeof option === 'boolean' || typeof option === 'symbol' || typeof option === 'bigint') {
+      optionsResult.push({ value: option, label: option });
+    } else {
+      optionsResult.push(option);
+    }
+  }
+
+  return optionsResult;
+})
 
 </script>
 
@@ -15,11 +27,11 @@ const props = defineProps<SelectInputProps>();
   <div class="flex flex justify-between items-center mb-3 text-sm">
     <label class="w-1/2 text-white">{{ props.label }}</label>
     <div class="w-1/2 relative inline-block">
-      <select class="w-full appearance-none bg-neutral-600 text-white px-3 py-2 rounded-md"
-      v-model="model">
+      <select class="w-full appearance-none bg-neutral-600 text-white px-3 py-2 rounded-md" v-model="model">
         <option value="" disabled>Select an option</option>
-        <template v-for="option in optionsFromSlot">
-          <option :value="option?.props?.value">{{ option?.children }}</option>
+        <slot></slot>
+        <template v-for="option in optionsComputed">
+          <option :value="option.value">{{ option.label }}</option>
         </template>
       </select>
       <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-white">
