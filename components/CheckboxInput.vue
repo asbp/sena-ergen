332e6524d681c71f344d861d1877a1c67d2dc648
asbp <asatriob@gmail.com>
2024-10-01 09:46:38 +0700

    fix checkbox input spacing

--- components/CheckboxInput.vue
+++ components/CheckboxInput.vue
@@ -11,13 +11,10 @@ const nameComputed = computed(() => {
 </script>
 
 <template>
-  <div class="mx-3">
-    <label class="relative h-6 w-full cursor-pointer flex items-center">
-      <input type="checkbox" :name="nameComputed" class="peer sr-only" v-model="model" />
-      <span
-        class="absolute h-full w-10 bg-neutral-800 rounded-full transition-all peer-checked:bg-cyan-200"></span>
-      <span class="absolute start-0 m-1 size-4 rounded-full bg-neutral-600 transition-all peer-checked:start-4"></span>
-      <span class="absolute start-12 text-sm flex items-center">{{ props.label }}</span>
-    </label>
-  </div>
+  <label class="relative h-6 w-full cursor-pointer flex items-center">
+    <input type="checkbox" :name="nameComputed" class="peer sr-only" v-model="model" />
+    <span class="absolute h-full w-10 bg-neutral-800 rounded-full transition-all peer-checked:bg-cyan-200"></span>
+    <span class="absolute start-0 m-1 size-4 rounded-full bg-neutral-600 transition-all peer-checked:start-4"></span>
+    <span class="absolute start-12 text-sm flex items-center">{{ props.label }}</span>
+  </label>
 </template>
