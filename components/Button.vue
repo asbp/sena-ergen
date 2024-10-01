1511246f7d537755c60a26b21f8d36ab22bad997
asbp <asatriob@gmail.com>
2024-10-01 23:14:35 +0700

    fix focus in components

--- components/Button.vue
+++ components/Button.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <button class="px-4 py-2 text-white text-sm rounded-lg bg-neutral-500 transition-all hover:bg-neutral-400 active:bg-neutral-700 outline-none focus:ring-1 focus:ring-offset-2 focus:ring-white">
+  <button class="px-4 py-2 text-white text-sm rounded-lg bg-neutral-500 transition-all hover:bg-neutral-400 active:bg-neutral-700 outline-none focus-visible:ring-1 focus-visible:ring-offset-2 focus-visible:ring-white">
     <slot> </slot>
   </button>
 </template>
--- components/TabView.vue
+++ components/TabView.vue
@@ -36,7 +36,8 @@ onMounted(() => {
   <div class="flex flex-col h-full">
     <div class="flex w-100 text-sm mb-3 bg-neutral-700 rounded first:rounded-s last:rounded-e">
       <template v-for="(tabPage, index) in tabPages">
-        <button class="flex-1 text-white p-3 transition-all outline-none focus:ring focus:ring-white first:rounded-s last:rounded-e"
+        <button
+          class="flex-1 text-white p-3 transition-all outline-none focus-visible:ring focus-visible:ring-white first:rounded-s last:rounded-e"
           @click="() => onTabClick(index)" :class="{ 'bg-neutral-500': activeTab === index }">
           {{ tabPage?.props?.title }}
         </button>
