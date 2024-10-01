08053a01287462ffaeebe05c2cc9830f03072b10
asbp <asatriob@gmail.com>
2024-10-01 10:44:15 +0700

    add sidebar button group

---
+++ components/SidebarRightButtonGroup.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="space-x-1">
+    <Button class="!px-2 !py-1 !rounded-sm !text-lg !bg-neutral-800 hover:!bg-neutral-600 active:!bg-neutral-800">
+      <i class="ti ti-book text-yellow-500"></i>
+    </Button>
+    <Button class="!px-2 !py-1 !rounded-sm !text-lg !bg-neutral-800 hover:!bg-neutral-600 active:!bg-neutral-800">
+      <i class="ti ti-stack-2 text-yellow-500"></i>
+    </Button>
+  </div>
+</template>
