631e72da203214604bfbf765e276944a93972aaf
asbp <asatriob@gmail.com>
2024-10-01 13:17:56 +0700

    add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,33 @@ export default defineNuxtConfig({
     typeCheck: true
   },
   css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      title: 'SENA',
+      link: [
+        {
+          rel: 'icon',
+          href: '/favicon-1-150x150.png',
+          sizes: '32x32'
+        },
+        {
+          rel: 'icon',
+          href: '/favicon-1-150x150.png',
+          sizes: '192x192'
+        },
+        {
+          rel: 'apple-touch-icon',
+          href: '/favicon-1-150x150.png',
+          sizes: '180x180'
+        },
+        {
+          rel: 'mask-icon',
+          href: '/favicon-1-150x150.png',
+          color: '#000000'
+        }
+      ]
+    }
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
