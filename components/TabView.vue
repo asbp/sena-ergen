9f72fb29831d350f22e47d2a17dc1aed22c17746
asbp <asatriob@gmail.com>
2024-10-01 09:39:58 +0700

    add click and change event on tab

--- components/TabView.vue
+++ components/TabView.vue
@@ -1,6 +1,10 @@
 <script lang="ts" setup>
 const slots = useSlots();
 const activeTab = ref(0);
+const emit = defineEmits<{
+  (e: 'tabClick', index: number): void,
+  (e: 'tabChange', index: number): void,
+}>();
 
 // Get the children of the TabView component
 const tabPages = computed(() => filterSlots(slots, 'TabPage'));
@@ -8,6 +12,16 @@ const tabPages = computed(() => filterSlots(slots, 'TabPage'));
 const setActiveTab = (index: number) => {
   activeTab.value = index;
 };
+
+const onTabClick = (index: number) => {
+  setActiveTab(index);
+
+  emit('tabClick', index);
+};
+
+watch(activeTab, (newIndex) => {
+  emit('tabChange', newIndex);
+});
 
 onMounted(() => {
   setActiveTab(0);
@@ -19,7 +33,7 @@ onMounted(() => {
   <div class="flex flex-col h-full">
     <div class="flex w-100 text-sm mb-3 bg-neutral-800">
       <template v-for="(tabPage, index) in tabPages">
-        <button class="flex-1 text-white p-3 transition-all" @click="() => setActiveTab(index)"
+        <button class="flex-1 text-white p-3 transition-all" @click="() => onTabClick(index)"
           :class="{ 'bg-neutral-500': activeTab === index }">
           {{ tabPage?.props?.title
           }}</button>
