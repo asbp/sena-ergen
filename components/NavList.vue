e06242e1251f2a8c1a7f69982abf27fd64a91b08
asbp <asatriob@gmail.com>
2024-10-01 23:30:27 +0700

    fix navlist spacing

--- components/NavList.vue
+++ components/NavList.vue
@@ -12,11 +12,11 @@ const props = withDefaults(defineProps<{
   <div
     class="flex flex-col xl:flex xl:flex-row grow items-center uppercase text-sm space-x-0 space-y-3 xl:space-x-3 xl:space-y-0 pb-3 xl:pb-0"
     :class="{ 'hidden': !isNavbarOpenOnNarrowViewport }">
-    <div class="flex flex-col xl:flex-row grow text-center text-white space-x-0 space-y-3 xl:space-x-3 xl:space-y-0 mt-3 xl:mt-0">
-      <a href="#" class="hover:text-white">Exposure Management</a>
-      <a href="#" class="hover:text-white">Exposure At Risk</a>
-      <a href="#" class="hover:text-white">Realtime Catastrophe Estimation</a>
-      <a href="#" class="!text-yellow-500">Report Generator</a>
+    <div class="flex flex-col xl:flex-row grow text-center text-white space-x-0 space-y-3 xl:space-x-3 xl:space-y-0 mt-3 xl:mt-0 h-full">
+      <a href="#" class="hover:text-white flex items-center justify-center h-full">Exposure Management</a>
+      <a href="#" class="hover:text-white flex items-center justify-center h-full">Exposure At Risk</a>
+      <a href="#" class="hover:text-white flex items-center justify-center h-full">Realtime Catastrophe Estimation</a>
+      <a href="#" class="!text-yellow-500 flex items-center justify-center h-full">Report Generator</a>
     </div>
     <SidebarRightButtonGroup />
     <div class="items-center justify-end pe-3 text-neutral-400 hidden xl:flex xl:ms-3 text-sm uppercase">
