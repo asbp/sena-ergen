b148c2d57ca74636f8e7534d6bf2a9b0655e76e4
asbp <asatriob@gmail.com>
2024-10-02 08:13:47 +0700

    add focus visible on checkbox

--- components/CheckboxInput.vue
+++ components/CheckboxInput.vue
@@ -13,7 +13,7 @@ const nameComputed = computed(() => {
 <template>
   <label class="relative h-6 w-full cursor-pointer flex items-center">
     <input type="checkbox" :name="nameComputed" class="peer sr-only" v-model="model" />
-    <span class="absolute h-full w-10 bg-neutral-800 rounded-full transition-all peer-checked:bg-cyan-200 peer-focus:ring peer-focus:ring-white"></span>
+    <span class="absolute h-full w-10 bg-neutral-800 rounded-full transition-all peer-checked:bg-cyan-200 peer-focus-visible:ring peer-focus-visible:ring-white"></span>
     <span class="absolute start-0 m-1 size-4 rounded-full bg-neutral-600 transition-all peer-checked:start-4"></span>
     <span class="absolute start-12 text-sm flex items-center">{{ props.label }}</span>
   </label>
